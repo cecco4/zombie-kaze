62af792e5a8a19eb4782eea391732d846c5f1d58
gattifrancesco94 <gattifrancesco94@c46089af-179b-bb54-b1d6-2ea1c394f08c>
2014-09-04 21:40:47 +0000

    debug aggiornato

--- trunk/src/main.cc
+++ trunk/src/main.cc
@@ -80,22 +80,64 @@ int main(void)
 		return -1;
 
 	//creo dysplay
-    al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
-    display = al_create_display(hschermo, lschermo);
+	#ifdef DEBUG_MODE
+        int scelta;
+        cout<<"Seleziona dimensione schermo: \n";
+        cout<<"1: 320x200\n";
+        cout<<"2: 640x400\n";
+        cout<<"3: 960x600\n";  
+        cout<<"4: auto-fullscreen\n";
+        cout<<"? ";
+        cin>>scelta;
 
-    int windowWidth = al_get_display_width(display);
-    int windowHeight = al_get_display_height(display);
-    int screenWidth = lschermo;
-    int screenHeight = hschermo;
+        if(scelta == 4)
+            al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
+        
+        display = al_create_display(lschermo*scelta, hschermo*scelta);
+        
+        int windowWidth, windowHeight;
+
+        if(scelta ==4) {
+            windowWidth = al_get_display_width(display);
+            windowHeight = al_get_display_height(display);
+        } else {
+            windowWidth = lschermo*scelta;
+            windowHeight = hschermo*scelta;
+        }
+
+        int screenWidth = lschermo;
+        int screenHeight = hschermo;
 
 
-    float sx = windowWidth / (float)screenWidth;
-    float sy = windowHeight / (float)screenHeight;
+        float sx = windowWidth / (float)screenWidth;
+        float sy = windowHeight / (float)screenHeight;
 
-    ALLEGRO_TRANSFORM trans;
-    al_identity_transform(&trans);
-    al_scale_transform(&trans, sx, sy);
-    al_use_transform(&trans);
+        ALLEGRO_TRANSFORM trans;
+        al_identity_transform(&trans);
+        al_scale_transform(&trans, sx, sy);
+        al_use_transform(&trans);        
+	#else
+
+        al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
+        display = al_create_display(lschermo, hschermo);
+
+        int windowWidth = al_get_display_width(display);
+        int windowHeight = al_get_display_height(display);
+        int screenWidth = lschermo;
+        int screenHeight = hschermo;
+
+
+        float sx = windowWidth / (float)screenWidth;
+        float sy = windowHeight / (float)screenHeight;
+
+        ALLEGRO_TRANSFORM trans;
+        al_identity_transform(&trans);
+        al_scale_transform(&trans, sx, sy);
+        al_use_transform(&trans);
+
+	#endif
+
+
 
 
 	if(!display)					//test display
